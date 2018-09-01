b84e16b3a288975416e3b44e9bc0904282105ac3
KishanChand <bskishanchand@gmail.com>
2018-09-01 21:11:32 +0530

    billing software

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SidebarComponent } from '.sidebar/sidebar.component'
 
 const routes: Routes = [];
 
